41e821afc784f31cba547135b96fd9b887cb3bfa
qs <khaled.dallah0@gmail.com>
2019-03-23 15:34:35 -0400

    Remove some trash

--- main.py
+++ main.py
@@ -2,27 +2,17 @@ import cv2
 import time
 from sun_tracking import * 
 
-
-
-cam = cv2.VideoCapture(1)
-
-print(cam.get(cv2.CAP_PROP_FPS))
-
-
-
-
+cam = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cam.read()
+    _, frame = cam.read()
     ref0,cur0 = sun_track(frame)
 
     #run controller 
     res=controller(ref0,cur0,10)
     print(res)
 
-    if not ret:
-        break
-
+    #close when ESC pressed
     k = cv2.waitKey(1)
     if k%256 == 27:
         # ESC pressed
